52568f98019221df4c8688e2fbd9a027bec74ef5
calones <34550332+I-Have-An-Issue@users.noreply.github.com>
2021-12-12 16:57:31 -0500

    merge Client.js changes

--- src/Client.js
+++ src/Client.js
@@ -12,42 +12,48 @@ class Server {
     start() {
         this._transport = net.Socket()
         // A connection has been made upstream
-        this._transport.on("connection", socket => {
-            // The server needs to send this data to the internal client
-            socket.on("data", msg => {
-                // Re-encode the data
-                let data = JSON.parse(msg.toString())
-                let buf = Buffer.from(data.msg, "base64")
+        this._transport.on("connect", () => console.log("connected to host"))
 
-                console.log(data.rinfo, data.msg.slice(0, 25))
+        this._transport.on("data", msg => {
+            // Re-encode the data
+            let data = []
+            for (let packet of msg.toString().split("[end]")) {
+                try { data.push(JSON.parse(packet)) }
+                catch (e) { console.log(packet) }
+            }
 
-                /*
-    
+            // Fixes strange buffer grouping
+            for (let packet of data) {
+                let buf = Buffer.from(packet.msg, "base64")
+                console.log(`[${packet.rinfo.address} -> localhost] ${packet.msg.slice(0, 45)}`)
+
                 // Check if that port is already open and send the data if it is
-                if (this._connections.has(data.rinfo.port)) this._connections.get(data.rinfo.port).send(buf, this._udpPort, "127.0.0.1")
-                else {
+                if (this._connections.has(packet.rinfo.port)) {
+                    this._connections.get(packet.rinfo.port).send(buf, this._udpPort, "127.0.0.1")
+                } else {
                     const sock = dgram.createSocket("udp4")
     
                     // If the internal client sends data, send that data to it's intended recipient
                     sock.on("message", (msg2, rinfo) => {
-                        socket.write(Buffer.from(
+                        console.log(`[${packet.rinfo.address} <- localhost] ${packet.msg.slice(0, 45)}`)
+                        this._transport.write(Buffer.from(
                             JSON.stringify({
-                                rinfo: data.rinfo, 
+                                rinfo: packet.rinfo, 
                                 msg: msg2.toString("base64")
                             })
                         ))
                     })
     
-                    sock.bind(data.rinfo.port)
+                    sock.bind()
     
                     // Send the data to the internal client
                     sock.send(buf, this._udpPort, "127.0.0.1")
-                    this._connections.set(data.rinfo.port, sock)
+                    this._connections.set(packet.rinfo.port, sock)
                 }
-                */
-            })
+            }
         })
 
+        this._transport.on("error", e => console.log(e))
         this._transport.connect(this._tcpPort, this._host)
     }
 }
