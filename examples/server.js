d1bcafec17059fd2518f5970312425473d4161d7
I-Have-An-Issue <34550332+I-Have-An-Issue@users.noreply.github.com>
2022-06-03 21:04:12 -0400

    ahdjfghvdsjkhgf

--- examples/server.js
+++ examples/server.js
@@ -4,8 +4,8 @@ const server = new Server(47127, 58727)
 server.on("connection", (ip) => console.log(`New client connected to transport socket: ${ip}`))
 server.on("error", (e) => console.log(e))
 
-server.on("data_in", (json) => console.log(json))
-server.on("data_out", (json) => console.log(json))
+//server.on("data_in", (json) => console.log(json))
+//server.on("data_out", (json) => console.log(json))
 
 server.once("tcp_listening", (port) => console.log(`TCP listening on port ${port}`))
 server.once("udp_listening", (port) => console.log(`UDP listening on port ${port}`))
